e38f4a4c26ab9d8953bfdea7fbc12cca1bc24897
Patrick Wu <wotingwu@live.com>
2018-08-31 09:22:02 +0800

    new set

---
+++ plugins/wsl-alias/wsl-alias.plugin.zsh
@@ -0,0 +1,13 @@
+# Basic
+alias cmd='cmd.exe'
+alias powershell='powershell.exe'
+alias winver='winver.exe'
+
+# WSL Relate
+alias wslconfig='wslconfig.exe'
+
+# Windows Specific
+alias code-win='code.exe'
+alias npm-win='npm.exe'
+alias node-win='node.exe'
+
